e115a898e4820b5b42c6ed991486980866548f8a
amir0900 <amir0900@gmail.com>
2020-07-15 17:49:18 +0300

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MyForm } from './form/form.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ MyForm, AppComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
