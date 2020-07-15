395da48aaf9aa23361cf77718e3c9807e91f0bff
amir0900 <amir0900@gmail.com>
2020-07-15 17:13:40 +0300

    1 file modified

--- app/todo/todos.component.spec.ts
+++ app/todo/todos.component.spec.ts
@@ -1,25 +1,25 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
-import { Observable } from 'rxjs';
+import { TodosComponent } from "./todos.component";
+import { TodoService } from "./todo.service";
+import { Observable } from "rxjs";
 
-describe('TodosComponent', () => {
+describe("TodosComponent", () => {
   let component: TodosComponent;
   let service: TodoService;
   beforeEach(() => {
     service = new TodoService(null);
-    component = new TodosComponent(service)
+    component = new TodosComponent(service);
   });
 
-  it('should return todo list from the server', () => {
+  it("should return todo list from the server", () => {
     // arrange
-    spyOn(service, 'getTodos').and.callFake(() =>{
-      return Observable.from([ [1,2,3] ])
-    })
+    spyOn(service, "getTodos").and.callFake(() => {
+      return Observable.from([[1, 2, 3]]);
+    });
 
-  // act
-  component.ngOnInit()
+    // act
+    component.ngOnInit();
 
-  // assert
-  expect(component.todos.length).toBeGreaterThan(0)
+    // assert
+    expect(component.todos.length).toBeGreaterThan(0);
   });
 });
