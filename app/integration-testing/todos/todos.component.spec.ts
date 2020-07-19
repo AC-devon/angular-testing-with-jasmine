41d7d56e3c157d92ea532bb8696ab7d467ebc057
amir0900 <amir0900@gmail.com>
2020-07-19 17:47:58 +0300

    1 file modified

--- app/integration-testing/todos/todos.component.spec.ts
+++ app/integration-testing/todos/todos.component.spec.ts
@@ -1,31 +1,30 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
 
-import { TodosComponent } from './todos.component';
-import { TodoService } from './todo.service';
-import { HttpModule } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { TodosComponent } from "./todos.component";
+import { TodoService } from "./todo.service";
+import { HttpModule } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 //NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
+// because the test will fail. This is because we have not
+// provided the TodoService as a dependency to TodosComponent.
+//
 // When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
+// to remove "x" from "xdescribe" below.
 
-xdescribe('TodosComponent', () => {
+describe("TodosComponent", () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpModule],
-      declarations: [ TodosComponent ],
-      providers:[TodoService]
-    })
-    .compileComponents();
+      declarations: [TodosComponent],
+      providers: [TodoService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -34,16 +33,16 @@ xdescribe('TodosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-    it('should get todo list from the server', () => {
-      // in module lvl
-      let todoService = TestBed.get(TodoService);
-      spyOn(todoService,'get').and.returnValue(Observable.from([ [1,2,3] ]))
-      // in component lvl
-     // let localInstaceTodos = fixture.debugElement.injector.get(TodoService)
+  it("should get todo list from the server", () => {
+    // in module lvl
+    let todoService = TestBed.get(TodoService);
+    spyOn(todoService, "getTodos").and.returnValue(Observable.from([[1, 2, 3]]));
+    // in component lvl
+    // let localInstaceTodos = fixture.debugElement.injector.get(TodoService)
 
     expect(component.todos.length).toBe(3);
   });
