bbce5da35c4041477d12fe8c946c04b2c5186d35
amir0900 <amir0900@gmail.com>
2020-07-19 15:37:10 +0300

    1 file modified

--- app/integration-testing/vote/voter.component.spec.ts
+++ app/integration-testing/vote/voter.component.spec.ts
@@ -1,32 +1,31 @@
-
-import { VoteComponent } from "./voter.component"
-import { TestBed, ComponentFixture } from "@angular/core/testing"
+import { VoteComponent } from "./voter.component";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 
-describe('vote component', () =>{
+describe("vote component", () => {
   let component: VoteComponent;
-  let fixture: ComponentFixture<VoteComponent>
-  
-  beforeEach(() =>{
+  let fixture: ComponentFixture<VoteComponent>;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations:[VoteComponent]
-    })
+      declarations: [VoteComponent]
+    });
 
-   fixture = TestBed.createComponent(VoteComponent);
-   component = fixture.componentInstance;
-  fixture.detectChanges();
-  //  fixture.nativeElement
-  //  fixture.debugElement
-  })
+    fixture = TestBed.createComponent(VoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    //  fixture.nativeElement
+    //  fixture.debugElement
+  });
 
-  it('should check if total vote in rendered', () =>{
+  it("should check if total vote in rendered", () => {
     component.othersVote = 21;
     component.myVote = 1;
-  fixture.detectChanges();
-  
-    let de = fixture.debugElement.query(By.css('.vote-count'));
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css(".vote-count"));
     let el: HTMLElement = de.nativeElement;
 
     expect(el.innerText).toContain(21);
-  })
-})
+  });
+});
