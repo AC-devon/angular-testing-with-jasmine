a231e577634e726b72c6ebc17fa78e68777a18ef
amir0900 <amir0900@gmail.com>
2020-07-19 15:26:14 +0300

    2 files modified

--- app/integration-test/voter.component.spec.ts
+++ app/integration-test/voter.component.spec.ts
@@ -1,6 +1,7 @@
 
 import { VoteComponent } from "./voter.component"
 import { TestBed, ComponentFixture } from "@angular/core/testing"
+import { By } from "@angular/platform-browser";
 
 describe('vote component', () =>{
   let component: VoteComponent;
@@ -13,8 +14,18 @@ describe('vote component', () =>{
 
    fixture = TestBed.createComponent(VoteComponent);
    component = fixture.componentInstance;
-
+  fixture.detectChanges();
   //  fixture.nativeElement
   //  fixture.debugElement
+  })
+
+  it('should check if total vote in rendered', () =>{
+    component.othersVote = 21;
+    component.myVote = 1;
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(21);
   })
 })
--- app/integration-test/voter.component.ts
+++ app/integration-test/voter.component.ts
@@ -1,19 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'my-voter',
-  template: ``,
-  styles: [ '' ]
+  selector: "my-voter",
+  template: `
+    <div class="voter">
+      <i
+        class="glyphicon glyphicon-menu-up vote-button"
+        [class.highlighted]="myVote == 1"
+        (click)="upVote()"
+      ></i>
+
+      <span class="vote-count">{{ totalVotes }}</span>
+
+      <i
+        class="glyphicon glyphicon-menu-down vote-button"
+        [class.highlighted]="myVote == -1"
+        (click)="downVote()"
+      ></i>
+    </div>
+  `,
+  styles: [""]
 })
-export class VoteComponent  {
+export class VoteComponent {
   @Input() othersVote = 0;
   @Input() myVote = 0;
 
   @Output() vote = new EventEmitter();
 
   upVote() {
-    if (this.myVote == 1)
-        return;
+    if (this.myVote == 1) return;
 
     this.myVote++;
 
@@ -21,9 +36,8 @@ export class VoteComponent  {
   }
 
   downVote() {
-    if (this.myVote == -1)
-        return;
-        
+    if (this.myVote == -1) return;
+
     this.myVote--;
 
     this.vote.emit({ myVote: this.myVote });
@@ -31,5 +45,5 @@ export class VoteComponent  {
 
   get totalVotes() {
     return this.othersVote + this.myVote;
-  } 
+  }
 }
