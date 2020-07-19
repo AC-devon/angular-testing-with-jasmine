1c8475435695ef8cac68655c3a79a90706fd11b1
amir0900 <amir0900@gmail.com>
2020-07-19 16:35:39 +0300

    1 file modified

--- app/integration-testing/vote/voter.component.spec.ts
+++ app/integration-testing/vote/voter.component.spec.ts
@@ -19,7 +19,7 @@ describe("vote component", () => {
   });
 
   it("should check if total vote in rendered", () => {
-    component.othersVote = 21;
+    component.othersVote = 20;
     component.myVote = 1;
     fixture.detectChanges();
 
@@ -27,5 +27,14 @@ describe("vote component", () => {
     let el: HTMLElement = de.nativeElement;
 
     expect(el.innerText).toContain(21);
+  });
+
+    it("should check if icon is hightlight", () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css(".glyphicon-menu-down"));
+
+    expect(de.classes['highlighted']).toBeTruthy();
   });
 });
